4b10eacec04ccd4b9a31dde59d8279c591280cfb
cb535 <cb535@cornell.edu>
2025-09-12 11:57:49 -0400

    get ready for section

--- sec03/PairTripleDetector2_GL.v
+++ sec03/PairTripleDetector2_GL.v
@@ -20,31 +20,9 @@ module PairTripleDetector2_GL
   //>'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
   // remove these lines before starting your implementation
-  // `ECE2300_UNUSED( a );
-  // `ECE2300_UNUSED( b );
-  // `ECE2300_UNDRIVEN( out );
-
-  wire detector0_out;
-
-  PairTripleDetector_GL detector0
-  (
-    .in0 (a[0]),
-    .in1 (a[1]),
-    .in2 (a[2]),
-    .out (detector0_out)
-  );
-
-  wire detector1_out;
-
-  PairTripleDetector_GL detector1
-  (
-    .in0 (b[0]),
-    .in1 (b[1]),
-    .in2 (b[2]),
-    .out (detector1_out)
-  );
-
-  or( out, detector0_out, detector1_out );
+  `ECE2300_UNUSED( a );
+  `ECE2300_UNUSED( b );
+  `ECE2300_UNDRIVEN( out );
 
 endmodule
 
