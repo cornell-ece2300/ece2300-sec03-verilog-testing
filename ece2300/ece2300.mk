2585d6b83f3cc9b2fe6b560c5fb6bb2b0ef25f35
cb535 <cb535@cornell.edu>
2026-09-10 22:15:32 -0400

    fix ece2300.mk

--- ece2300/ece2300.mk
+++ ece2300/ece2300.mk
@@ -3,10 +3,3 @@
 #=========================================================================
 
 ece2300_srcs = \
-  SevenSegFL.v \
-  TestReadOnlyMemory.v \
-
-# ece2300_tests = \
-#   TestReadOnlyMemory-test.v \
-
-# $(eval $(call check_part,ece2300))
